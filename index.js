225bbb6154a6cbc96ed031cb663d0448ed14e83f
dancun616 <dancun616@gmail.com>
2022-11-22 13:58:53 +0300

    completed message

---
+++ index.js
@@ -0,0 +1,43 @@
+function subtract(a,b) {
+  console.log(a-b);
+  return(a-b) ;
+}
+console.log(subtract(a,b));
+function divide(a, b) {
+  console.log(a/b);
+  return(a/b) ;
+}
+console.log(divide(a, b));
+function add(a, b) {
+  console.log(a+b);
+  return(a+b) ;
+}
+console.log(add(a,b));
+function multiply(a, b) {
+  console.log(a*b);
+  return(a*b);
+}
+console.log(multiply(a, b));
+function increment(a) {
+  console.log(a+1);
+  return(a+1);
+}
+console.log(increment(a));
+function decrement(a) {
+  console.log(a-1);
+  return(a-1);
+}
+console.log(decrement(a));
+function makeInt(a) {
+  console.log(a);
+  return(a);
+}
+console.log(makeInt(a.toString()));
+function makeInt(string)   {
+  console.log(parseInt(string, 10));
+  return(parseInt(string, 10));
+}
+console.log(makeInt('0x2328'));
+function preserveDecimal(string) 
+  console.log(parseInt(string));
+  return (parseInt(string));
